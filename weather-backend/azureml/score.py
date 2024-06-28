fee70aa72bdb40cf47c362830cb4ed1e3a355820
Lalariniaina Prisca Ramanantoanina <lalariniaina-prisca.ramanantoanina@epita.fr>
2024-06-28 17:27:41 +0200

    feat(weather-backend): change classification model

--- weather-backend/azureml/score.py
+++ weather-backend/azureml/score.py
@@ -1,28 +1,25 @@
+from predict import initialize, predict_url, predict_image
+from PIL import Image
+import numpy as np
+
 import base64
 import io
 import json
-
-import numpy as np
-from PIL import Image
-from predict import initialize, predict_image, predict_url
-
 
 # Azure ML model loader
 def init():
     initialize()
 
-
 # Helper to predict an image encoded as base64
 def predict_image_base64(encoded_image):
-    if encoded_image.startswith("b'"):
+    if encoded_image.startswith('b\''):
         encoded_image = encoded_image[2:-1]
 
-    decoded_img = base64.b64decode(encoded_image.encode("utf-8"))
-    img_buffer = io.BytesIO(decoded_img)
+    decoded_img = base64.b64decode(encoded_image.encode('utf-8'))
+    img_buffer  = io.BytesIO(decoded_img)
 
     image = Image.open(io.BytesIO(decoded_img))
     return predict_image(image)
-
 
 # Azure ML entry point
 def run(json_input):
@@ -38,10 +35,9 @@ def run(json_input):
             results = predict_image_base64(image)
         else:
             raise Exception("Invalid input. Expected url or image")
-        return results
+        return (results)
     except Exception as e:
-        return str(e)
-
+        return (str(e))
 
 if __name__ == "__main__":
     init()
@@ -53,8 +49,8 @@ if __name__ == "__main__":
     image.save(imgio, "PNG")
     imgio.seek(0)
     dataimg = base64.b64encode(imgio.read())
-    input = '{"image": "' + dataimg.decode("utf-8") + '"}'
-    print("calling")
+    input = '{"image": "' + dataimg.decode('utf-8') + '"}'
+    print ("calling")
     result = run(input)
     input_url = '{"url": "https://raw.githubusercontent.com/Microsoft/Cognitive-CustomVision-Windows/master/Samples/Images/Test/test_image.jpg" }'
     run(input_url)
